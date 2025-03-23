f1590725051aabb6d20fc706528e150ac4d22a76
sejon <wkdtpwhs@gmail.com>
2025-03-23 20:04:00 +0900

    #32 SSE 기능 개발 :: thread pool 갯수 조절 property 로 이동

--- src/main/java/com/chatforyou/io/config/SchedulerConfig.java
+++ src/main/java/com/chatforyou/io/config/SchedulerConfig.java
@@ -1,6 +1,7 @@
 package com.chatforyou.io.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,16 @@ import java.util.concurrent.ScheduledExecutorService;
 @Configuration
 @Slf4j
 public class SchedulerConfig {
+
+    @Value("${spring.thread.bound.multi}")
+    private int ioBoundMultiplier;
+
     @Bean
     public ScheduledExecutorService scheduledExecutorService() {
-        log.info("############ Usable ScheduledThreadPool :: {} ", Runtime.getRuntime().availableProcessors());
-        return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+        int corePoolSize = Runtime.getRuntime().availableProcessors();
+        log.info("############## ScheduledThreadPool Configuration :: Cores={}, PoolSize={}",
+                corePoolSize, corePoolSize * ioBoundMultiplier);
+
+        return Executors.newScheduledThreadPool(corePoolSize * ioBoundMultiplier);
     }
 }
